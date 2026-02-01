71865f35625bbd0ce501d20f30dc691ca9033e23
JunCodera1 <minhtien0601200@gmail.com>
2026-02-01 12:22:28 +0700

    feat: Initial project setup for the Japanese learning web application.

---
+++ src/test/java/com/example/japanweb/TestJapanWebApplication.java
@@ -0,0 +1,11 @@
+package com.example.japanweb;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestJapanWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(JapanWebApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
